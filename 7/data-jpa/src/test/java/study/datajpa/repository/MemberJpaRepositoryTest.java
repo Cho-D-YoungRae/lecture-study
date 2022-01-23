7de03ceaefdb1556af0daecc8dcd95f0d01e3900
Cho-D-YoungRae <yrc9229@gmail.com>
2022-01-23 15:03:38 +0900

    7-2

--- 7/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ 7/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
