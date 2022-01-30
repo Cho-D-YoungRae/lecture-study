d6e253fd81628aa35427799c8c3dd958ef895751
Cho-D-YoungRae <yrc9229@gmail.com>
2022-01-30 14:33:21 +0900

    9. 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 START

---
+++ 9/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -0,0 +1,16 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslApplication.class, args);
+	}
+}
---
+++ 9/querydsl/src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,28 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
