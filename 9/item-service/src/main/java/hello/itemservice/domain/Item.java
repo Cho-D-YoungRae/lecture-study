211fbd890ecc87177f2049ff0cda19f179b958a3
Cho-D-YoungRae <yrc9229@gmail.com>
2022-02-08 21:51:15 +0900

    9-7

---
+++ 9/item-service/src/main/java/hello/itemservice/domain/Item.java
@@ -0,0 +1,22 @@
+package hello.itemservice.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
