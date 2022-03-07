7f79f1bff9ee1d30a1182c47db2d2079eaa03f7a
Cho-D-YoungRae <yrc9229@gmail.com>
2022-03-07 16:17:09 +0900

    5-3

--- 5/ex-hello-jpa-2/src/main/java/hellojpa/Member.java
+++ 5/ex-hello-jpa-2/src/main/java/hellojpa/Member.java
@@ -1,7 +1,6 @@
 package hellojpa;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,6 +8,8 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member {
 
     @Id
