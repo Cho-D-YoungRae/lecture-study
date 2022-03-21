47c99bae6c28f747699d1926eb5af7dd7b5acd3a
Cho-D-YoungRae <yrc9229@gmail.com>
2022-03-21 12:06:24 +0900

    ONGOING: 7 - 2

--- 7/data-jpa-2/src/main/java/study/datajpa2/entity/Member.java
+++ 7/data-jpa-2/src/main/java/study/datajpa2/entity/Member.java
@@ -2,22 +2,43 @@ package study.datajpa2.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (Objects.nonNull(team)) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
