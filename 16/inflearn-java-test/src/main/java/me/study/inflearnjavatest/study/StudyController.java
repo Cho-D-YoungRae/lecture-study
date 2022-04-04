acd3264874c152e58422750b1c8c53b6a1648be2
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-04 23:20:53 +0900

    CHORE: 16. 프로젝트 디렉토리 이동

---
+++ 16/inflearn-java-test/src/main/java/me/study/inflearnjavatest/study/StudyController.java
@@ -0,0 +1,24 @@
+package me.study.inflearnjavatest.study;
+
+import lombok.RequiredArgsConstructor;
+import me.study.inflearnjavatest.domain.Study;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StudyController {
+
+    private final StudyRepository repository;
+
+    @GetMapping("/study/{id}")
+    public Study getStudy(@PathVariable Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Study not found for '" + id + "'"));
+    }
+
+    @PostMapping("/study")
+    public Study createsStudy(@RequestBody Study study) {
+        return repository.save(study);
+    }
+
+}
