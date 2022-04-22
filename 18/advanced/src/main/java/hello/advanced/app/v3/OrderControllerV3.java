47dd37a6ab9d0f20897bb4e5069599426f63cc88
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-22 22:21:59 +0900

    18: ~2

---
+++ 18/advanced/src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,33 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v3")
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+
+    private final LogTrace trace;
+
+    @GetMapping("/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져주어야 한다. -> 예외를 먹어버리면 흐름이 바뀜 (정상흐름으로)
+        }
+    }
+}
