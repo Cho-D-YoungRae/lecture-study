a78de63515ee70f7cbe9cef1408f0149e8fe02a6
Cho-D-YoungRae <yrc9229@gmail.com>
2022-04-25 15:50:58 +0900

    18: ~ 6

---
+++ 18/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,24 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy start");
+        long startTime = System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args);
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy end resultTime={}", resultTime);
+
+        return result;
+    }
+}
