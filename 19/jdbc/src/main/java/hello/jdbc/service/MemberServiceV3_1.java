eab0b1bc75e0c6860fa4edc4b6600dab4a0aa7bb
Cho-D-YoungRae <yrc9229@gmail.com>
2022-05-26 16:50:50 +0900

    19: 4

---
+++ 19/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,54 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static java.util.Objects.nonNull;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            // 비즈니스 로직
+            bizLogic(fromId, toId, money);
+            transactionManager.commit(status); // 성공시 커밋
+        } catch (Exception e) {
+            transactionManager.rollback(status); // 실패시 롤백
+            throw new IllegalStateException(e);
+        }
+        // 트랜잭션 커밋되거나 롤백될 때 내부에서 release 해줌 -> 커밋되거나 롤백되면 다 끝난 것이므로
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validate(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validate(Member toMember) {
+        if ("ex".equals(toMember.getMemberId())) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
---
+++ 19/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,39 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validate(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validate(Member toMember) {
+        if ("ex".equals(toMember.getMemberId())) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
