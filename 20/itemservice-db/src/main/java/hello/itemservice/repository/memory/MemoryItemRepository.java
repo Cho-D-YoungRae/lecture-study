b3af3d4f4f5e0f89aeeda0877436875b425f7ae3
Cho-D-YoungRae <yrc9229@gmail.com>
2022-06-10 12:46:45 +0900

    20: 1

---
+++ 20/itemservice-db/src/main/java/hello/itemservice/repository/memory/MemoryItemRepository.java
@@ -0,0 +1,64 @@
+package hello.itemservice.repository.memory;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ObjectUtils;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+@Repository
+public class MemoryItemRepository implements ItemRepository {
+
+    private static final Map<Long, Item> store = new ConcurrentHashMap<>(); //static
+    private static final AtomicLong sequence = new AtomicLong(0); //static
+
+    @Override
+    public Item save(Item item) {
+        item.setId(sequence.addAndGet(1));
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = findById(itemId).orElseThrow();
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+        return store.values().stream()
+                .filter(item -> {
+                    if (ObjectUtils.isEmpty(itemName)) {
+                        return true;
+                    }
+                    return item.getItemName().contains(itemName);
+                }).filter(item -> {
+                    if (maxPrice == null) {
+                        return true;
+                    }
+                    return item.getPrice() <= maxPrice;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+
+}
