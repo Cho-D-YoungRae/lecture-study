919b33c1847a50ccacedf1cef6d416f229c0d351
Cho-D-YoungRae <yrc9229@gmail.com>
2022-06-14 17:06:15 +0900

    20: 9

---
+++ 20/springtx/src/main/java/hello/springtx/order/OrderService.java
@@ -0,0 +1,38 @@
+package hello.springtx.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    // JPA 는 트랜잭션 커밋 시점에 Order 데이터를 DB 에 반영
+    @Transactional
+    public void order(Order order) throws NotEnoughMoneyException {
+        log.info("order 호출");
+        orderRepository.save(order);
+
+        log.info("결제 프로세스 진입");
+        if ("예외".equals(order.getUsername())) {
+            log.info("시스템 예외 발생");
+            throw new RuntimeException("시스템 예외");
+
+        } else if ("잔고부족".equals(order.getUsername())) {
+            log.info("잔고 부족 비즈니스 예외 발생");
+            order.setPayStatus("대기");
+            throw new NotEnoughMoneyException("잔고가 부족 합니다.");
+
+        } else {
+            // 정상 승인
+            log.info("정상 승인");
+            order.setPayStatus("완료");
+        }
+        log.info("결제 프로세스 완료");
+    }
+}
