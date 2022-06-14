97a729775de5fa2505a800e106d7e83bd5bc9339
Cho-D-YoungRae <yrc9229@gmail.com>
2022-06-14 12:40:00 +0900

    20: 8

--- 20/itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ 20/itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,7 +1,6 @@
 package hello.itemservice.repository.jpa;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
@@ -9,14 +8,11 @@ import hello.itemservice.domain.QItem;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
---
+++ 20/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
@@ -0,0 +1,52 @@
+package hello.itemservice.repository.v2;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.QItem;
+import hello.itemservice.repository.ItemSearchCond;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Repository
+public class ItemQueryRepositoryV2 {
+
+    private final JPAQueryFactory query;
+
+    public ItemQueryRepositoryV2(EntityManager em) {
+        this.query = new JPAQueryFactory(em);
+    }
+
+    public List<Item> findAll(ItemSearchCond cond) {
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        return query
+                .selectFrom(QItem.item)
+                .where(
+                        likeItemName(itemName),
+                        maxPrice(maxPrice))
+                .fetch();
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if (nonNull(maxPrice)) {
+            return QItem.item.price.loe(maxPrice);
+        }
+        return null;
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if (StringUtils.hasText(itemName)) {
+            return QItem.item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
+}
