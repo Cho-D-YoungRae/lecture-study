1b06d3eeb765255ce3fa10f6123d07293f1c7e6f
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-04 23:26:44 +0900

    17: 1-1

---
+++ 17/basic-security-2/src/main/java/io/security/basicsecurity2/BasicSecurity2Application.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSecurity2Application.class, args);
+    }
+
+}
---
+++ 17/basic-security-2/src/test/java/io/security/basicsecurity2/BasicSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.basicsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
