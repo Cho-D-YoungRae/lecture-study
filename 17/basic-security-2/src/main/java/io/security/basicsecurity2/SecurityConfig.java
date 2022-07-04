1970f93dc8e31d2a0d17965ed4b5704cb402d440
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-05 00:07:05 +0900

    17: 1-2

---
+++ 17/basic-security-2/src/main/java/io/security/basicsecurity2/SecurityConfig.java
@@ -0,0 +1,26 @@
+package io.security.basicsecurity2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+
+                .and()
+                .formLogin()
+
+                .and().build();
+    }
+}
