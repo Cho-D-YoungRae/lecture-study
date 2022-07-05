ec5ff13734b46db5c42113c59f22c8b8c43f0626
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-05 11:53:39 +0900

    17: 1-3

--- 17/basic-security-2/src/main/java/io/security/basicsecurity2/SecurityConfig.java
+++ 17/basic-security-2/src/main/java/io/security/basicsecurity2/SecurityConfig.java
@@ -1,17 +1,20 @@
 package io.security.basicsecurity2;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    // 1-3 Form Login 인증
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -20,7 +23,23 @@ public class SecurityConfig {
 
                 .and()
                 .formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login-proc")
+                .successHandler((request, response, authentication) -> {
+                    log.info("authentication={}", authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.error("exception", exception);
+                    response.sendRedirect("/login");
+                })
+                .permitAll()    // 해당 로그인 페이지로는 접근이 가능하도록
 
-                .and().build();
+                .and()
+                .build();
     }
 }
