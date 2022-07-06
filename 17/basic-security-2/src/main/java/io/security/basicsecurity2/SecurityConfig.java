184ab37a99d1139b85a12c0e928d89fca5b2b655
Cho-D-YoungRae <yrc9229@gmail.com>
2022-07-06 15:55:40 +0900

    17: 1-6 ~ 1-7

--- 17/basic-security-2/src/main/java/io/security/basicsecurity2/SecurityConfig.java
+++ 17/basic-security-2/src/main/java/io/security/basicsecurity2/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Slf4j
@@ -14,16 +15,48 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
 
-    /*
-    // 1-3 Form Login 인증
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        rememberMe(http);
         return http
                 .authorizeRequests()
                 .anyRequest().authenticated()
 
                 .and()
+                .formLogin()
+
+                .and()
+                .build();
+    }
+
+    // 1-6) Remember Me 인증
+    // 1-7) Remember Me 인증 필터 : RememberMeAuthenticationFilter
+    private HttpSecurity rememberMe(HttpSecurity http) throws Exception {
+        return http
+                .rememberMe()
+//                .rememberMeParameter("remember")
+//                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService)
+                .and();
+    }
+
+    // 1-5) Logout 처리, LogoutFilter
+    private HttpSecurity logout(HttpSecurity http) throws Exception {
+        return http
+                .logout()
+                .logoutUrl("/logout")   // default
+                .logoutSuccessUrl("/login") // default : login?logout
+                .addLogoutHandler((request, response, authentication) -> request.getSession().invalidate())
+                .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
+                .deleteCookies("remember-me")
+                .and();
+    }
+
+    // 1-3 Form Login 인증
+    private HttpSecurity formLogin(HttpSecurity http) throws Exception {
+        return http
                 .formLogin()
 //                .loginPage("/loginPage")
                 .defaultSuccessUrl("/")
@@ -40,45 +73,6 @@ public class SecurityConfig {
                     response.sendRedirect("/login");
                 })
                 .permitAll()    // 해당 로그인 페이지로는 접근이 가능하도록
-
-                .and()
-                .build();
-    }
-    */
-    /*
-    // 1-4 Form Login 인증 필터 : UsernamePasswordAuthenticationFilter
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-
-                .and()
-                .formLogin()
-
-                .and()
-                .build();
-    }
-    */
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-
-                .and()
-                .formLogin()
-
-                .and()
-                .logout()
-                .logoutUrl("/logout")   // default
-                .logoutSuccessUrl("/login") // default : login?logout
-                .addLogoutHandler((request, response, authentication) -> request.getSession().invalidate())
-                .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
-                .deleteCookies("remember-me")
-
-                .and()
-                .build();
+                .and();
     }
 }
