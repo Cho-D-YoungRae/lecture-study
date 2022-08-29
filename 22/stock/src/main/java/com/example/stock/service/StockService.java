8ca1066bba1b324c7d2e8b2e9114433c4c787625
Cho-D-YoungRae <yrc9229@gmail.com>
2022-08-29 12:06:35 +0900

    22. 섹션 2

--- 22/stock/src/main/java/com/example/stock/service/StockService.java
+++ 22/stock/src/main/java/com/example/stock/service/StockService.java
@@ -14,7 +14,7 @@ public class StockService {
     private final StockRepository stockRepository;
 
     @Transactional
-    public void decrease(Long id, Long quantity) {
+    public synchronized void decrease(Long id, Long quantity) {
         // get stock
         // 재고 감소
         // 저장
