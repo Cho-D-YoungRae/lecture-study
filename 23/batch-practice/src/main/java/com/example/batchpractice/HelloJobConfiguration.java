cf843cbcbb81184e9ef783e3f75d8038fa8ea8c1
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-20 17:12:14 +0900

    23: ~2-3

---
+++ 23/batch-practice/src/main/java/com/example/batchpractice/HelloJobConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.batchpractice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(helloStep1())
+                .next(helloStep2())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1() {
+        return stepBuilderFactory.get("helloStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("=".repeat(30));
+                    System.out.println(" >> Hello Spring Batch!!");
+                    System.out.println("=".repeat(30));
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2() {
+        return stepBuilderFactory.get("helloStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("=".repeat(30));
+                    System.out.println(" >> Step2 was executed");
+                    System.out.println("=".repeat(30));
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
