c4d0bc33937dd4e77d074951389da2b567ef1347
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-20 12:42:49 +0900

    23: ~ 2-2

---
+++ 23/batch-practice/src/test/java/com/example/batchpractice/BatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
