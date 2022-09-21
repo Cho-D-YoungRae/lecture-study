1724588d55e7d8902dc5c258db9f3a7a0fb44e98
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-21 17:41:07 +0900

    23: ~ 3-2

--- 23/batch-practice/src/main/java/com/example/batchpractice/DBJobConfiguration.java
+++ 23/batch-practice/src/main/java/com/example/batchpractice/DBJobConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/**
+ * ~ 2-5
+ */
+//@Configuration
 @RequiredArgsConstructor
 public class DBJobConfiguration {
 
