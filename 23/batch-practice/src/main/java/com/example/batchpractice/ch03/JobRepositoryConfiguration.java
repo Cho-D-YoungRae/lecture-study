22f73cede026a34402bd910ad6883d102ede2c49
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-24 15:10:26 +0900

    23: ~ 3-10

--- 23/batch-practice/src/main/java/com/example/batchpractice/ch03/JobRepositoryConfiguration.java
+++ 23/batch-practice/src/main/java/com/example/batchpractice/ch03/JobRepositoryConfiguration.java
@@ -5,54 +5,50 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
- * 3-9
+ * 3-10
  */
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class ExecutionContextConfiguration {
+public class JobRepositoryConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
 
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final JobRepository jobRepository;
+
     @Bean
     public Job batchJob() {
-        return jobBuilderFactory.get("job")
+        return jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
-                .next(step3())
-                .next(step4())
+                .listener(new JobRepositoryListener(jobRepository))
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new ExecutionContextTasklet1())
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step1 was executed");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new ExecutionContextTasklet2())
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(new ExecutionContextTasklet3())
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet(new ExecutionContextTasklet4())
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> step2 was executed");
+                    return RepeatStatus.FINISHED;
+                })
                 .build();
     }
 
---
+++ 23/batch-practice/src/main/java/com/example/batchpractice/ch03/JobRepositoryListener.java
@@ -0,0 +1,41 @@
+package com.example.batchpractice.ch03;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.repository.JobRepository;
+
+import static java.util.Objects.nonNull;
+
+/**
+ * 3-10
+ */
+@RequiredArgsConstructor
+public class JobRepositoryListener implements JobExecutionListener {
+
+    private final JobRepository jobRepository;
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+        JobParameters jobParameters = jobExecution.getJobParameters();
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addString("requestDate", "20210102")
+//                .toJobParameters();
+        JobExecution lastJobExecution = jobRepository.getLastJobExecution(jobName, jobParameters);
+        if (nonNull(lastJobExecution)) {
+            for (StepExecution stepExecution : lastJobExecution.getStepExecutions()) {
+                BatchStatus status = stepExecution.getStatus();
+                System.out.println("status = " + status);
+                ExitStatus exitStatus = stepExecution.getExitStatus();
+                System.out.println("exitStatus = " + exitStatus);
+                String stepName = stepExecution.getStepName();
+                System.out.println("stepName = " + stepName);
+            }
+        }
+    }
+}
