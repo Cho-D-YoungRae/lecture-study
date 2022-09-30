835258aa93fffa77276d7416ab0be027894cb853
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-30 12:36:48 +0900

    23: ~ 4-6

--- 23/batch-practice/src/main/java/com/example/batchpractice/ch04/ch06/StartNextConfiguration.java
+++ 23/batch-practice/src/main/java/com/example/batchpractice/ch04/ch06/StartNextConfiguration.java
@@ -1,16 +1,19 @@
-package com.example.batchpractice.ch04.ch05;
+package com.example.batchpractice.ch04.ch06;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.*;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 @RequiredArgsConstructor
-public class SimpleJobConfiguration {
+public class StartNextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
 
@@ -22,20 +25,6 @@ public class SimpleJobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
-                .incrementer(new RunIdIncrementer())
-                .validator(parameters -> {})
-                .preventRestart()
-                .listener(new JobExecutionListener() {
-                    @Override
-                    public void beforeJob(JobExecution jobExecution) {
-
-                    }
-
-                    @Override
-                    public void afterJob(JobExecution jobExecution) {
-
-                    }
-                })
                 .build();
     }
 
