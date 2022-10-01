b3b753c63e97aa7ddcbcab0a6119ab8fe459e118
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-01 15:07:34 +0900

    23: ~ 5-2

--- 23/batch-practice/src/main/java/com/example/batchpractice/ch05/ch01/StepBuilderConfiguration.java
+++ 23/batch-practice/src/main/java/com/example/batchpractice/ch05/ch01/StepBuilderConfiguration.java
@@ -11,11 +11,10 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.*;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class StepBuilderConfiguration {
 
---
+++ 23/batch-practice/src/main/java/com/example/batchpractice/ch05/ch02/TaskletStepConfiguration.java
@@ -0,0 +1,65 @@
+package com.example.batchpractice.ch05.ch02;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class TaskletStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return jobBuilderFactory.get("batchJob")
+                .start(taskletStep())
+                .next(chunkStep())
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    public Step taskletStep() {
+        return stepBuilderFactory.get("step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step chunkStep() {
+        return stepBuilderFactory.get("chunkStep")
+                .<String, String>chunk(10)
+                .reader(new ListItemReader<>(List.of("item1", "item2", "item3", "item4", "item5")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return item.toLowerCase();
+                    }
+                })
+                .writer(new ItemWriter<String>() {
+                    @Override
+                    public void write(List<? extends String> items) throws Exception {
+                        items.forEach(item -> System.out.println("item = " + item));
+                    }
+                })
+                .build();
+    }
+
+}
