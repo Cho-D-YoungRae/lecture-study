b3bc376b2b83f5f5f2ad63e0f09c0a5d8c27b464
Cho-D-YoungRae <yrc9229@gmail.com>
2022-12-25 20:26:28 +0900

    21: ~ 섹션 4. 글 내용 화면 만들기

--- 21/hodolog/front/src/views/HomeView.vue
+++ 21/hodolog/front/src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import axios from "axios";
 import {ref} from "vue";
+import {useRouter} from "vue-router";
+
+const router = useRouter();
 
 const posts = ref([])
 
@@ -9,13 +12,19 @@ axios.get("/api/posts?page=1&size=5").then(response =>
       posts.value.push(post)
     })
 );
+
+const moveToRead = () => {
+  router.push("read")
+}
 </script>
 
 <template>
   <ul>
     <li v-for="post in posts" :key="post.id">
       <div>
-        {{ post.title }}
+<!--        router-link 를 이용해서 싱글페이지로 화면만 이동되도록 할 수 있다.-->
+<!--        a 태그를 사용하면 애플리케이션 전체를 다시 불러오게 됨-->
+        <router-link :to="{ name: 'read', params: { postId: post.id } }">{{ post.title }}</router-link>
       </div>
       <div>
         {{ post.content }}
---
+++ 21/hodolog/front/src/views/ReadView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {defineProps, onMounted, ref} from "vue";
+import axios from "axios";
+
+const props = defineProps({
+  postId: {
+    type: [Number, String],
+    require: true
+  }
+});
+
+const post = ref({
+  id: 0,
+  title: "",
+  content: ""
+});
+
+onMounted(() => {
+  axios.get(`/api/posts/${props.postId}`).then(response => {
+    post.value = response.data;
+  });
+})
+</script>
+
+<template>
+  <h2>{{post.title}}</h2>
+  <div>{{post.content}}</div>
+</template>
+
+<style scoped>
+
+</style>
