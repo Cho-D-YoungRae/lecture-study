6c00692169f2ac58eb40c99ce497a379e63c673e
Cho Youngrae <yrc9229@gmail.com>
2023-09-07 16:48:58 +0900

    스프링 시큐리티 OAuth2 - Spring Boot 기반으로 개발하는 Spring Security OAuth2 : sec1.초기화 과정 이해 - SecurityBuilder / SecurityConfigurer

--- 28/spring-security-oauth2-practice/src/main/java/io/security/oauth2/springsecurityoauth2practice/sec1springsecurityfundamentals/securityconfigurer/CustomSecurityConfigurer.java
+++ 28/spring-security-oauth2-practice/src/main/java/io/security/oauth2/springsecurityoauth2practice/sec1springsecurityfundamentals/securityconfigurer/CustomSecurityConfigurer.java
@@ -1,6 +1,34 @@
 package io.security.oauth2.springsecurityoauth2practice.sec1springsecurityfundamentals.securityconfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-public class CustomSecurityConfigurer extends AbstractHttpConfigurer {
+@Slf4j
+public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+
+    private boolean secured;
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+        super.init(builder);
+        log.info("init method started...");
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+        log.info("configure method started....");
+
+        if (secured) {
+            log.info("https is required");
+        } else {
+            log.info("https is optional");
+        }
+    }
+
+    public CustomSecurityConfigurer setFlag(boolean secured) {
+        this.secured = secured;
+        return this;
+    }
 }
--- 28/spring-security-oauth2-practice/src/main/java/io/security/oauth2/springsecurityoauth2practice/sec1springsecurityfundamentals/securityconfigurer/SecurityConfig.java
+++ 28/spring-security-oauth2-practice/src/main/java/io/security/oauth2/springsecurityoauth2practice/sec1springsecurityfundamentals/securityconfigurer/SecurityConfig.java
@@ -1,11 +1,22 @@
 package io.security.oauth2.springsecurityoauth2practice.sec1springsecurityfundamentals.securityconfigurer;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(config -> config
+                        .anyRequest().authenticated()
+                )
+                .apply(new CustomSecurityConfigurer()
+                        .setFlag(false))
+        ;
+        return http.build();
+    }
 }
