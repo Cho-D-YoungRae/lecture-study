1e7f9165ef46cbb57a2e4abc2eef4bccd50a1558
Cho Youngrae <yrc9229@gmail.com>
2023-09-13 23:12:25 +0900

    스프링 시큐리티 OAuth2: sec6

--- 28/oauth2-client/src/main/java/com/example/oauth2client/IndexController.java
+++ 28/oauth2-client/src/main/java/com/example/oauth2client/IndexController.java
@@ -1,13 +1,25 @@
 package com.example.oauth2client;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/client-registrations/{registrationId}")
+    public ClientRegistration getClientRegistration(@PathVariable String registrationId) {
+        return clientRegistrationRepository.findByRegistrationId(registrationId);
     }
 }
