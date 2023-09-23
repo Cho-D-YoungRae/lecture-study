5879f9b8282498d54993f463c4915afdc78b4f27
Cho Youngrae <yrc9229@gmail.com>
2023-09-24 00:17:33 +0900

    스프링 시큐리티 OAuth2: sec12

--- 28/resource-server/src/main/java/com/example/resourceserver/ResourceServerConfig.java
+++ 28/resource-server/src/main/java/com/example/resourceserver/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.example.resourceserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,9 @@ public class ResourceServerConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(config -> config
                 .anyRequest().authenticated()
+        );
+        http.oauth2ResourceServer(config -> config
+                .jwt(Customizer.withDefaults())
         );
         return http.build();
     }
