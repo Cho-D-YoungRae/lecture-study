dc7950ea181789e0fc6454b8d32f5fea661139af
Cho Youngrae <yrc9229@gmail.com>
2023-10-12 19:05:29 +0900

    스프링 시큐리티 OAuth2: sec13. RSA 검증 기능 구현 - PublicKey.txt 에 의한 검증

--- 28/resource-server/src/main/java/com/example/resourceserver/config/OAuth2ResourceServerConfig.java
+++ 28/resource-server/src/main/java/com/example/resourceserver/config/OAuth2ResourceServerConfig.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,15 +43,15 @@ public class OAuth2ResourceServerConfig {
         http.userDetailsService(userDetailsService());
         http.addFilterBefore(jwtAuthenticationFilter(null, null), UsernamePasswordAuthenticationFilter.class);
 //        http.addFilterBefore(jwtAuthorizationMacFilter(null), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(jwtAuthorizationRsaFilter(null), UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(jwtAuthorizationRsaFilter(null), UsernamePasswordAuthenticationFilter.class);
         // JwtDecoder 사용
-//        http.oauth2ResourceServer(config -> config
-//                .jwt(Customizer.withDefaults())
-//        );
+        http.oauth2ResourceServer(config -> config
+                .jwt(Customizer.withDefaults())
+        );
         return http.build();
     }
 
-    @Bean
+//    @Bean
     public JwtAuthorizationRsaFilter jwtAuthorizationRsaFilter(RSAKey rsaKey) throws JOSEException {
         return new JwtAuthorizationRsaFilter(new RSASSAVerifier(rsaKey.toRSAPublicKey()));
     }
