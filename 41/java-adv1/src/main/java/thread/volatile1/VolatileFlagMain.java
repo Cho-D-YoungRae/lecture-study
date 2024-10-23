728f75896b105411d8900a4cb51b1663b222eea1
Youngrae Cho <yrc9229@gmail.com>
2024-10-23 17:00:40 +0900

    김영한의 실전 자바 - 고급 1편: ~6

---
+++ 41/java-adv1/src/main/java/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,35 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag 를 false 로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+
+//        boolean runFlag = true;
+         volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+            while (runFlag) {
+                // runFlag 가 false로 변하면 탈출
+            }
+            log("task 종료");
+        }
+    }
+}
