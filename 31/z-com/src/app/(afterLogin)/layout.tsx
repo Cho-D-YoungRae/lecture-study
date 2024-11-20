3c2bb84e2c6b102bd73016c4499af6ffad52989b
Youngrae Cho <yrc9229@gmail.com>
2024-11-20 11:52:25 +0900

    Next + React Query로 SNS 서비스 만들기: 오른쪽 섹션 클론코딩

--- 31/z-com/src/app/(afterLogin)/layout.tsx
+++ 31/z-com/src/app/(afterLogin)/layout.tsx
@@ -1,10 +1,12 @@
-import {ReactNode} from "react";
+import React, {ReactNode} from "react";
 import style from './layout.module.css';
 import Link from "next/link";
 import Image from "next/image";
 import zLogo from '@/../public/zlogo.png';
 import NavMenu from "@/app/(afterLogin)/_component/NavMenu";
 import LogoutButton from "@/app/(afterLogin)/_component/LogoutButton";
+import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
+import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
 
 export default function AfterLoginLayout(
   {children}: Readonly<ReactNode>
@@ -33,15 +35,23 @@ export default function AfterLoginLayout(
         <div className={style.rightSectionInner}>
           <main className={style.main}>{children}</main>
           <section className={style.rightSection}>
-            <form className={style.search}>
-              <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
-                <g>
-                  <path
-                    d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
-                </g>
-              </svg>
-              <input type="search"/>
-            </form>
+            <div style={{ marginBottom: 60, width: 'inherit' }}>
+              <form className={style.search}>
+                <svg width={20} viewBox="0 0 24 24" aria-hidden="true">
+                  <g>
+                    <path
+                      d="M10.25 3.75c-3.59 0-6.5 2.91-6.5 6.5s2.91 6.5 6.5 6.5c1.795 0 3.419-.726 4.596-1.904 1.178-1.177 1.904-2.801 1.904-4.596 0-3.59-2.91-6.5-6.5-6.5zm-8.5 6.5c0-4.694 3.806-8.5 8.5-8.5s8.5 3.806 8.5 8.5c0 1.986-.682 3.815-1.824 5.262l4.781 4.781-1.414 1.414-4.781-4.781c-1.447 1.142-3.276 1.824-5.262 1.824-4.694 0-8.5-3.806-8.5-8.5z"></path>
+                  </g>
+                </svg>
+                <input type="search"/>
+              </form>
+            </div>
+            <TrendSection/>
+            <div className={style.followRecommend}>
+              <FollowRecommend/>
+              <FollowRecommend/>
+              <FollowRecommend/>
+            </div>
           </section>
         </div>
       </div>
