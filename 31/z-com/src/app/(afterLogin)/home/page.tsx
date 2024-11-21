1c7a658c4deb50072462b465a7f3ab190b6c7a98
Youngrae Cho <yrc9229@gmail.com>
2024-11-21 11:46:24 +0900

    Next + React Query로 SNS 서비스 만들기: PostForm 만들기(타이핑 외우기)

--- 31/z-com/src/app/(afterLogin)/home/page.tsx
+++ 31/z-com/src/app/(afterLogin)/home/page.tsx
@@ -1,13 +1,15 @@
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import style from './home.module.css';
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
+import React from "react";
+import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 
 export default function Home() {
   return (
     <main className={style.main}>
       <TabProvider>
         <Tab/>
-        {/*<PostForm/>*/}
+        <PostForm />
         {/*<Post/>*/}
         {/*<Post/>*/}
         {/*<Post/>*/}
