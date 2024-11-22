0e3ad1b12b5e5839decbbe7ab7c12623b21cbc52
Youngrae Cho <yrc9229@gmail.com>
2024-11-22 12:52:20 +0900

    Next + React Query로 SNS 서비스 만들기: usePathname과 /explore 페이지

--- 31/z-com/src/app/(afterLogin)/_component/TrendSection.tsx
+++ 31/z-com/src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import style from './trendSection.module.css';
 import Trend from "@/app/(afterLogin)/_component/Trend";
+import {usePathname} from "next/navigation";
 
 
 export default function TrendSection() {
+  const pathname = usePathname();
+  if (pathname === '/explore') {
+    return null;
+  }
   return (
     <div className={style.trendBg}>
       <div className={style.trend}>
@@ -19,5 +26,5 @@ export default function TrendSection() {
         <Trend/>
       </div>
     </div>
-  )
+  );
 }
--- 31/z-com/src/app/(afterLogin)/explore/page.tsx
+++ 31/z-com/src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,25 @@
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Trend from "@/app/(afterLogin)/_component/Trend";
+import style from "./explore.module.css";
+
 export default function Home() {
   return (
-    <main>
-      검색 페이지
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm/>
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+        <Trend/>
+      </div>
     </main>
   );
 }
