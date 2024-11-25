051b1d5d6d53d987fb0dcaa18bdc10234b53f1f2
Youngrae Cho <yrc9229@gmail.com>
2024-11-25 14:10:41 +0900

    Next + React Query로 SNS 서비스 만들기: ## useSearchParams와 프로필, /search 페이지

--- 31/z-com/src/app/(afterLogin)/search/page.tsx
+++ 31/z-com/src/app/(afterLogin)/search/page.tsx
@@ -1,8 +1,41 @@
+import style from './search.module.css';
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Tab from "@/app/(afterLogin)/search/_component/Tab";
+import Post from "@/app/(afterLogin)/_component/Post";
 
-export default function Home() {
+type Props = {
+  searchParams: { q: string, f?: string, pf?: string };
+}
+
+export default function Search({ searchParams }: Props) {
   return (
-    <div>
-      검색 페이지
-    </div>
-  );
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton/>
+          </div>
+          <div className={style.formZone}>
+            <SearchForm q={searchParams.q} />
+          </div>
+        </div>
+        <Tab/>
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
+  )
 }
