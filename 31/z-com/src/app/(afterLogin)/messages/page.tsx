ef38001fd3a4cbcfb2e7319079d045195cd650fe
Youngrae Cho <yrc9229@gmail.com>
2024-11-27 12:06:22 +0900

    Next + React Query로 SNS 서비스 만들기: /messages 페이지 라이브 클론

--- 31/z-com/src/app/(afterLogin)/messages/page.tsx
+++ 31/z-com/src/app/(afterLogin)/messages/page.tsx
@@ -1,7 +1,18 @@
+import style from './message.module.css';
+import Room from "@/app/(afterLogin)/messages/_component/Room";
+
 export default function Home() {
   return (
-    <main>
-      쪽지 페이지
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
     </main>
-  );
+  )
 }
