97855bb96ba3d50686ae9ca327ed64f80796f2ad
Youngrae Cho <yrc9229@gmail.com>
2025-01-14 13:42:41 +0900

    Next + React Query로 SNS 서비스 만들기: finish

--- 32/5_가위바위보/useInterval.js
+++ 32/5_가위바위보/useInterval.js
@@ -1,18 +1,11 @@
 import { useRef, useEffect } from 'react';
 
-// const [running, setRunning] = useState(true);
-// useInterval(() => {
-//   console.log('안녕');
-// }, running ? 1000 : null);
-//
-// callback 을 왜 tick 이라는 함수로 감쌌는지?
-// callback 이 바뀌어도 새로 setInterval 이 안되지만 최신 callback 을 참조 가능
 function useInterval(callback, delay) {
   const savedCallback = useRef();
 
   useEffect(() => {
     savedCallback.current = callback;
-  });
+  }, [callback]);
 
   useEffect(() => {
     function tick() {
