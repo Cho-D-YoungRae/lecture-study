633e0cf0b5ea568ccf0e13ae6da9fd9d170db559
Youngrae Cho <yrc9229@gmail.com>
2025-03-26 09:30:23 +0900

    서버개발자 과제전형 완벽가이드 - 1편: 프로젝트 세팅

---
+++ 43/library-search/src/main/kotlin/com/lecture/librarysearch/LibrarySearchApplication.kt
@@ -0,0 +1,11 @@
+package com.lecture.librarysearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibrarySearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<LibrarySearchApplication>(*args)
+}
---
+++ 43/library-search/src/test/kotlin/com/lecture/librarysearch/LibrarySearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lecture.librarysearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibrarySearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
