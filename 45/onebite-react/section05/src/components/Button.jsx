e8aae133fd1c4187f16cb25abda7fa9b9eba54a1
Youngrae Cho <yrc9229@gmail.com>
2025-06-18 10:23:30 +0900

    한 입 크기로 잘라 먹는 리액트: 5.5

--- 45/onebite-react/section05/src/components/Button.jsx
+++ 45/onebite-react/section05/src/components/Button.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
 function Button({children, text, color = "black"}) {
+  const onClickButton = () => {
+    console.log(`Button clicked: ${text}`);
+  };
+  // 이벤트 객체
+  // SyntheticBaseEvent(합성 이벤트): 모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태
+  const onMouseEnterButton = (e) => {
+    console.log(e);
+    console.log(`Mouse entered: ${text}`);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      onMouseEnter={onMouseEnterButton}
+      style={{ color: color }}
+    >
       {text} - {color}
       {children}
     </button>
