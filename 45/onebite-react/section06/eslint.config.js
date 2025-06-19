748876dfa802c27db04a9fa9aa5340721d03b4da
Youngrae Cho <yrc9229@gmail.com>
2025-06-19 11:59:52 +0900

    한 입 크기로 잘라 먹는 리액트: 6.1

---
+++ 45/onebite-react/section06/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      // 'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // 학습을 위한 설정
+      'no-unused-vars': "off",
+      "react/prop-types": "off",
+    },
+  },
+]
---
+++ 45/onebite-react/section06/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (<>
+    counter app
+  </>);
+}
+
+export default App
