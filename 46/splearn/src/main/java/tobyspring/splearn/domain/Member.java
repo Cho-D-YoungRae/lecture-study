554b7e6d37f35ed1a4b852715de2311445e6cc0d
Youngrae Cho <yrc9229@gmail.com>
2025-06-23 13:51:54 +0900

    토비의 클린 스프링 - 도메인 모델 패턴과 헥사고날 아키텍처 Part 1: ~25

--- 46/splearn/src/main/java/tobyspring/splearn/domain/Member.java
+++ 46/splearn/src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,22 +1,28 @@
 package tobyspring.splearn.domain;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.NaturalIdCache;
 
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
 
 @Entity
+@NaturalIdCache
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NaturalId
+    @Embedded
     private Email email;
 
     private String nickname;
--- 46/splearn/src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
+++ 46/splearn/src/test/java/tobyspring/splearn/application/required/MemberRepositoryTest.java
@@ -4,9 +4,11 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import tobyspring.splearn.domain.Member;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static tobyspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static tobyspring.splearn.domain.MemberFixture.createPasswordEncoder;
 
@@ -30,5 +32,16 @@ class MemberRepositoryTest {
         assertThat(member.getId()).isNotNull();
 
         entityManager.flush();
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        Member member1 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member1);
+
+        Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 }
