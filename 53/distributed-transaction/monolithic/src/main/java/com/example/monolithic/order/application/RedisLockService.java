84fba51a0b18299c5a9b299e177be5c357b11741
Cho-D-YoungRae <yrc9229@gmail.com>
2025-10-06 16:50:50 +0900

    주문시스템으로 알아보는 분산트랜잭션: ~10

---
+++ 53/distributed-transaction/monolithic/src/main/java/com/example/monolithic/order/application/RedisLockService.java
@@ -0,0 +1,31 @@
+package com.example.monolithic.order.application;
+
+import org.slf4j.Logger;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@Service
+public class RedisLockService {
+
+    private static final Logger log = getLogger(RedisLockService.class);
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public RedisLockService(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public boolean tryLock(String key, String value) {
+        log.info("try lock for key: {}, value: {}", key, value);
+        return Objects.requireNonNull(stringRedisTemplate.opsForValue().setIfAbsent(key, value));
+    }
+
+    public void releaseLock(String key) {
+        log.info("release lock for key: {}", key);
+        stringRedisTemplate.delete(key);
+    }
+}
--- 53/distributed-transaction/monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
+++ 53/distributed-transaction/monolithic/src/main/java/com/example/monolithic/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.monolithic.order.controller;
 
 import com.example.monolithic.order.application.OrderService;
+import com.example.monolithic.order.application.RedisLockService;
 import com.example.monolithic.order.application.dto.CreateOrderResult;
 import com.example.monolithic.order.controller.dto.CreateOrderRequest;
 import com.example.monolithic.order.controller.dto.CreateOrderResponse;
@@ -13,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+    private final RedisLockService redisLockService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, RedisLockService redisLockService) {
         this.orderService = orderService;
+        this.redisLockService = redisLockService;
     }
 
     @PostMapping("/orders")
@@ -26,6 +29,16 @@ public class OrderController {
 
     @PostMapping("/orders/place")
     public void placeOrder(@RequestBody PlaceOrderRequest request) {
-        orderService.placeOrder(request.toPlaceOrderCommand());
+        String key = "order:monolithic:" + request.orderId();
+        boolean acquiredLock = redisLockService.tryLock(key, request.orderId().toString());
+        if (!acquiredLock) {
+            throw new IllegalStateException("락획득에 실패했습니다.");
+        }
+
+        try {
+            orderService.placeOrder(request.toPlaceOrderCommand());
+        } finally {
+            redisLockService.releaseLock(key);
+        }
     }
 }
