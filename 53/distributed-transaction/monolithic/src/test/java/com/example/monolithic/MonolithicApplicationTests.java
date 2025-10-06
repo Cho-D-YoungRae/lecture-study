65f2957acae01a5dfa930c19ef681825ff173426
Cho-D-YoungRae <yrc9229@gmail.com>
2025-10-06 14:43:13 +0900

    주문시스템으로 알아보는 분산트랜잭션: ~3

---
+++ 53/distributed-transaction/monolithic/src/test/java/com/example/monolithic/MonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
